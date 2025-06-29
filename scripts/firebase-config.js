ade2019eab49d9a9203c9a65ca0a8628b3e81c49
Mogan-ram <moganram10@gmail.com>
2025-06-29 09:23:52 +0530

    apikeys_hardcoded

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,18 +1,20 @@
+// scripts/firebase-config.js
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
-// Use environment variables injected by Netlify
 const firebaseConfig = {
-    apiKey: window.env.FIREBASE_API_KEY,
-    authDomain: window.env.FIREBASE_AUTH_DOMAIN,
-    projectId: window.env.FIREBASE_PROJECT_ID,
-    storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: window.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyCHYvtRtMn0fJPkPAXs8csufnxbmXZUU2c",
+    authDomain: "studentteacherappointmen-9cad2.firebaseapp.com",
+    projectId: "studentteacherappointmen-9cad2",
+    storageBucket: "studentteacherappointmen-9cad2.appspot.com",
+    messagingSenderId: "418499813102",
+    appId: "1:418499813102:web:8403882306391c54fa9dff"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
